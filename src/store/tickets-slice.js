78a4961086cd60d2900cbec179863a3111b732e5
Re-Dnor <MokshinRE@yandex.ru>
2022-08-09 13:34:44 +0500

    new get path

--- src/store/tickets-slice.js
+++ src/store/tickets-slice.js
@@ -10,7 +10,7 @@ const initialState = {
 export const loadTickets = createAsyncThunk(
   '@@tickets/loading',
   () => {
-    return axios.get('http://localhost:8000/tickets')
+    return axios.get('https://tickets-app-test-work.herokuapp.com/')
   })
 
 const ticketsSlice = createSlice({
