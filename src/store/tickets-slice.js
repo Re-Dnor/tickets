56f9b96bf66ac199a71c4d1598952cd72034b46a
Re-Dnor <MokshinRE@yandex.ru>
2022-08-09 13:31:12 +0500

    add server

--- src/store/tickets-slice.js
+++ src/store/tickets-slice.js
@@ -10,7 +10,7 @@ const initialState = {
 export const loadTickets = createAsyncThunk(
   '@@tickets/loading',
   () => {
-    return axios.get('http://localhost:4000/tickets')
+    return axios.get('http://localhost:8000/tickets')
   })
 
 const ticketsSlice = createSlice({
